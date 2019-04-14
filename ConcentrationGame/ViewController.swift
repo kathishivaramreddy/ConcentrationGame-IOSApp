4453cc596c0a2d82dad3622fee1a0342110729e6
ssh <kathishivaram@gmail.com>
2019-04-14 20:47:29 +0530

    add cards and flip functionality

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -14,7 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    let emojiArray = ["👻","🧛🏿‍♀️","👻","🧛🏿‍♀️"]
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        
+        let cardNumber = cardButtons.firstIndex(of: sender)
+        flipCard(withEmoji: emojiArray[cardNumber!], on: sender)
+    }
+    
+    func flipCard(withEmoji emoji : String, on button : UIButton){
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 
 }
 
