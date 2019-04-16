fe7133f3867c2ff8eae4a96ea7883facb073e56a
ssh <kathishivaram@gmail.com>
2019-04-16 17:18:20 +0530

    cleaning up code

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -55,10 +55,15 @@ class ViewController: UIViewController {
     
     private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
         }
         return emoji[card.identifier] ?? "?"
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        return Int(arc4random_uniform(UInt32(self)))
     }
 }
 
