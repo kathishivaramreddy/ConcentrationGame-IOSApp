a0a854151c52059ffca8752f9eeadfab9f24b6f0
ssh <kathishivaram@gmail.com>
2019-04-19 16:27:42 +0530

    add tabbar view and splitview

--- ConcentrationGame/Controller/ConcentrationThemeChooserViewController.swift
+++ ConcentrationGame/Controller/ConcentrationThemeChooserViewController.swift
@@ -23,6 +23,7 @@ class ConcentrationThemeChooserViewController: UIViewController {
     ]
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         if segue.identifier == "Choose Theme" {
             if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
                 if let cvc = segue.destination as? ConcentrationViewController {
